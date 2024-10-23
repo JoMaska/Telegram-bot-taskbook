7ed2c422fbb45da4e04d2c214ffcd463825ace0e
Maska <jojomas900@gmail.com>
2024-10-23 14:21:53 +0300

    add admins_id in config

--- src/config/config.py
+++ src/config/config.py
@@ -4,11 +4,12 @@ from dataclasses import dataclass
 @dataclass
 class TgBot:
     token: str
+    admins_id: str
     
 @dataclass
 class Database:
     url: str
-    
+
 @dataclass
 class Config:
     bot: TgBot
@@ -19,7 +20,8 @@ def load_config(path: str | None = None):
     env.read_env(path)
     return Config(
         bot=TgBot(
-            token=env("BOT_TOKEN")
+            token=env("BOT_TOKEN"),
+            admins_id=env("ADMINS_ID")
             ),
         db=Database(
             url=env("DB_URL")
